8264b641481256acc27717935dedf3e24098a6f6
Himanshu Choudhary <himanshukakran8@gmail.com>
2025-10-01 14:03:20 +0530

    .gitignore

--- myfunProject1/src/app.js
+++
@@ -1,6 +0,0 @@
-
-require('./functions/HelloFunc');
-require('./functions/MyQueueFunc');
-require('./functions/MyTimerFunc');
-require('./functions/MyEventGridFunc');
-require('./functions/QueueOutputFunc');
